66eba4859955072d6f84064157bc79b4be6e29ef
Gema Parreño <gema.parreno.piqueras@gmail.com>
2018-05-29 20:40:33 +0200

    Update create an agent.py

--- Creating_an_agent.py
+++ Creating_an_agent.py
@@ -1,16 +1,4 @@
-# Copyright 2017 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS-IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+## Kudos StevenBrown 
 """A base agent to write custom scripted agents."""
 
 from __future__ import absolute_import
@@ -18,7 +6,8 @@ from __future__ import division
 from __future__ import print_function
 
 from pysc2.lib import actions
-import time 
+from pysc2.agents import base_agent
+from pysc2.lib import features 
 
 
 # Functions
@@ -26,8 +15,11 @@ _BUILD_PYLON = actions.Function.Build_Pylon_screen.id
 _NOOP = actions.Function.no_op.id
 _SELECT_POINT = actions.Function.select_point.id
 _MOVE_CAMERA = actions.Function.move_camera.id
+_BUILD_GATEWAY = actions.Function.Build_Gateway_screen.id
 
 # Features
+_PLAYER_RELATIVE = features.ScreenFeatures.player_relative.index
+_UNIT_TYPE = features.ScreenFeatures.unit_type.index
 
 #Unit IDs
 _PROTOSS_NEXUS = 59
@@ -40,10 +32,22 @@ _NOT_QUEUED = [0]
 _QUEUED = [1]
 
 class BaseAgent(object):
-  """A base agent to write custom scripted agents.
-  It can also act as a passive agent that does nothing but no-ops.
-  """
+  base_top_left = None
+  pylon_built = False
+  probe_selected = False
 
+  
+  def transformLocation(self, x, x_distance, y, y_distance):
+      if not self.base_top_left:
+          return [x - x_distance, y - y_distance]
+        
+      return [x + x_distance, y + y_distance]  
+  
+  if self.base_top_left is None:
+    player_y, player_x = (obs.observation["minimap"][_PLAYER_RELATIVE] == _PLAYER_SELF).nonzero()
+    self.base_top_left = player_y.mean() <= 47
+    
+    
   def __init__(self):
     self.reward = 0
     self.episodes = 0
@@ -59,6 +63,41 @@ class BaseAgent(object):
     self.episodes += 1
 
   def step(self, obs):
-    self.steps += 1
-    self.reward += obs.reward
-    return actions.FunctionCall(actions.FUNCTIONS.no_op.id, [])
+      super(SimpleAgent, self).step(obs)
+        
+      time.sleep(0.5)
+        
+      if self.base_top_left is None:
+          player_y, player_x = (obs.observation["minimap"][_PLAYER_RELATIVE] == _PLAYER_SELF).nonzero()
+          self.base_top_left = player_y.mean() <= 31
+            
+      if not self.supply_depot_built:
+          if not self.scv_selected:
+              unit_type = obs.observation["screen"][_UNIT_TYPE]
+              unit_y, unit_x = (unit_type == _PROTOSS_PROBE).nonzero()
+                
+              target = [unit_x[0], unit_y[0]]
+                
+              self.scv_selected = True
+                
+              return actions.FunctionCall(_SELECT_POINT, [_NOT_QUEUED, target])
+          elif _BUILD_PYLON in obs.observation["available_actions"]:
+              unit_type = obs.observation["screen"][_UNIT_TYPE]
+              unit_y, unit_x = (unit_type == _PROTOSS_NEXUS).nonzero()
+                
+              target = self.transformLocation(int(unit_x.mean()), 0, int(unit_y.mean()), 20)
+                
+              self.supply_depot_built = True
+                
+              return actions.FunctionCall(_BUILD_PYLON, [_NOT_QUEUED, target])
+      elif not self.barracks_built and _BUILD_GATEWAY in obs.observation["available_actions"]:
+          unit_type = obs.observation["screen"][_UNIT_TYPE]
+          unit_y, unit_x = (unit_type == _PROTOSS_NEXUS).nonzero()
+            
+          target = self.transformLocation(int(unit_x.mean()), 20, int(unit_y.mean()), 0)
+            
+          self.barracks_built = True
+            
+          return actions.FunctionCall(_BUILD_GATEWAY, [_NOT_QUEUED, target])
+
+      return actions.FunctionCall(_NOOP, [])
