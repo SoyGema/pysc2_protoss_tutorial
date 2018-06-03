56644befba98ea99a003ce81fd3d116508dd39db
Gema Parreño <gema.parreno.piqueras@gmail.com>
2018-06-03 15:03:32 +0200

    Update custom_agent.py
    -- Able to do pylon and Gateway in bottom_right
    -- Relative positions is really painful

--- Custom_agent.py
+++ Custom_agent.py
@@ -1,11 +1,10 @@
-
-## Kudos to StevenBrown 
-# This is a scripted bot experiment to try the new release of pysc2 
-# The original tutorial by Steven Brown with Terran can be found here 
+## Kudos to StevenBrown
+# This is a scripted bot experiment to try the new release of pysc2
+# The original tutorial by Steven Brown with Terran can be found here
 # https://chatbotslife.com/building-a-basic-pysc2-agent-b109cde1477c
 
-# The bot has been built with Protoss race 
-## At this point this scripted bot is able to select a probe and build a pylon 
+# The bot has been built with Protoss race
+## At this point this scripted bot is able to select a probe and build a pylon
 
 """A base agent to write custom scripted agents."""
 
@@ -15,7 +14,7 @@ from __future__ import print_function
 
 from pysc2.lib import actions
 from pysc2.agents import base_agent
-from pysc2.lib import features 
+from pysc2.lib import features
 import time
 import numpy
 
@@ -28,90 +27,90 @@ _BUILD_GATEWAY = actions.FUNCTIONS.Build_Gateway_screen.id
 
 # Features
 _UNIT_TYPE = features.SCREEN_FEATURES.unit_type.index
-
+_POWER = features.SCREEN_FEATURES.power.index
 _PLAYER_SELF = 1
 
-#Unit IDs
+# Unit IDs
 _PROTOSS_NEXUS = 59
 _PROTOSS_PROBE = 84
 _PROTOSS_PYLON = 60
 _PROTOSS_GATEWAY = 62
 
-#Parameters
+# Parameters
 _PLAYER_SELF = 1
 _SUPPLY_USED = 3
 _SUPPLY_MAX = 4
 _NOT_QUEUED = [0]
 _QUEUED = [1]
 
+
 class SimpleAgent(base_agent.BaseAgent):
-  base_top_left = None
-  pylon_built = False
-  probe_selected = False
-  gateway_built = False
-  gateway_selected = False
-  gateway_rallied = False
+    base_top_left = None
+    pylon_built = False
+    probe_selected = False
+    gateway_built = False
+    gateway_selected = False
+    gateway_rallied = False
 
-  def _xy_locs(mask):
-      """Mask should be a set of bools from comparison with a feature layer."""
-      y, x = mask.nonzero()
-      return list(zip(x, y))
+    def _xy_locs(mask):
+        """Mask should be a set of bools from comparison with a feature layer."""
+        y, x = mask.nonzero()
+        return list(zip(x, y))
 
+    def step(self, obs):
+        super(SimpleAgent, self).step(obs)
 
-
-  def step(self, obs):
-      super(SimpleAgent, self).step(obs)
-
-      def _xy_locs(mask):
-          """Mask should be a set of bools from comparison with a feature layer."""
-          y, x = mask.nonzero()
-          return list(zip(x, y))
-
-      #if self.base_top_left is None:
-          #player_y, player_x = (obs.observation['minimap'][_PLAYER_RELATIVE] == _PLAYER_SELF).nonzero()
-          #self.base_top_left = player_y.mean() <= 47
-
-      if self.base_top_left is None:
-          PLAYER_RELATIVE = obs.observation.feature_screen.player_relative
-          player_ = _xy_locs(PLAYER_RELATIVE == _PLAYER_SELF)
-          self.base_top_left = numpy.mean(player_, axis=0).round() <= 31
-
-      if not self.pylon_built:
-          if not self.probe_selected:
-              unit_type = obs.observation["feature_screen"][_UNIT_TYPE]
-              unit_y, unit_x = (unit_type == _PROTOSS_PROBE).nonzero()
-              target = [unit_x[0], unit_y[0]]
-                
-              self.probe_selected = True
-                
-              return actions.FunctionCall(_SELECT_POINT, [_NOT_QUEUED, target])
-
-          elif _BUILD_PYLON in obs.observation["available_actions"]:
-              unit_type = obs.observation["feature_screen"][_UNIT_TYPE]
-              unit_y, unit_x = (unit_type == _PROTOSS_NEXUS).nonzero()
+        def _xy_locs(mask):
+            """Mask should be a set of bools from comparison with a feature layer."""
+            y, x = mask.nonzero()
+            return list(zip(x, y))
 
 
-              if self.base_top_left[0] == True:
-                  target = [unit_x[0]+5, unit_y[0]+5]
-              else:
-                  target = [unit_x[0]+10, unit_y[0]-5]
+        if self.base_top_left is None:
+            PLAYER_RELATIVE = obs.observation.feature_screen.player_relative
+            player_ = _xy_locs(PLAYER_RELATIVE == _PLAYER_SELF)
+            self.base_top_left = numpy.mean(player_, axis=0).round() <= 31
 
-              self.pylon_built = True
-                
-              return actions.FunctionCall(_BUILD_PYLON, [_NOT_QUEUED, target])
-      elif not self.gateway_built and _BUILD_GATEWAY in obs.observation["available_actions"]:
-          unit_type = obs.observation["feature_screen"][_UNIT_TYPE]
-          unit_y, unit_x = (unit_type == _PROTOSS_NEXUS).nonzero()
+        if not self.pylon_built:
+            if not self.probe_selected:
+                unit_type = obs.observation.feature_screen[_UNIT_TYPE]
+                unit_y, unit_x = (unit_type == _PROTOSS_PROBE).nonzero()
+                target = [unit_x[0], unit_y[0]]
 
+                self.probe_selected = True
 
+                return actions.FunctionCall(_SELECT_POINT, [_NOT_QUEUED, target])
 
-          if self.base_top_left[0] == True:
-              target = [unit_x[0] + 5, unit_y[0] + 5]
-          else:
-              target = [unit_x[0] + 10, unit_y[0] - 5]
-            
-          self.gateway_built = True
-            
-          return actions.FunctionCall(_BUILD_GATEWAY, [_NOT_QUEUED, target])
+            elif _BUILD_PYLON in obs.observation["available_actions"]:
+                unit_type = obs.observation.feature_screen[_UNIT_TYPE]
+                unit_y, unit_x = (unit_type == _PROTOSS_NEXUS).nonzero()
 
-      return actions.FunctionCall(_NOOP, [])
+                if self.base_top_left[0] == True:
+                    target = [unit_x[0] + 5, unit_y[0] + 5]
+                else:
+                    target = [unit_x[0] + 10, unit_y[0] - 5]
+
+                self.pylon_built = True
+
+                return actions.FunctionCall(_BUILD_PYLON, [_NOT_QUEUED, target])
+        if not self.gateway_built and self.pylon_built and _BUILD_GATEWAY in obs.observation["available_actions"]:
+            print('try building gateway')
+            area = obs.observation.feature_screen[_POWER].nonzero()
+            if len(area[0]) != 0:
+                print(area)
+                unit_type = obs.observation.feature_screen[_UNIT_TYPE]
+                unit_y, unit_x = (unit_type == _PROTOSS_PYLON).nonzero()
+                print(unit_x)
+
+                if self.base_top_left[0] == True:
+                    print("area is top left")
+                    target = [unit_x[0]+12, unit_y[0]]
+                else:
+                    print("area is bottom right")
+                    target = [unit_x[0]+12, unit_y[0]]
+
+                self.gateway_built = True
+
+                return actions.FunctionCall(_BUILD_GATEWAY, [_NOT_QUEUED, target])
+
+        return actions.FunctionCall(_NOOP, [])
